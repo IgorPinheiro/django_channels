7dbe5e8a2a12ee6827efd3dd25da95a9ad31209b
Igor <igor.warlog@gmail.com>
2024-11-12 22:12:13 -0300

    Configurações iniciais do websocket mais criação da home

--- testes_channels/asgi.py
+++ testes_channels/asgi.py
@@ -1,16 +1,33 @@
-"""
-ASGI config for testes_channels project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import meu_app.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testes_channels.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teste_channels.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            meu_app.routing.websocket_urlpatterns
+        )
+    )
+})
+
+
+
+
+
+
+
+
+
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testes_channels.settings')
+
+# application = get_asgi_application()
